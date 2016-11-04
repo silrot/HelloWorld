500da080936ebcf58d3153023174a75f9bc368d7
silrot <resetmara3@gmail.com>
2016-11-04 10:16:18 +0900

    hotfix

--- Helloworld/src/com/usnschool/HelloWorld.java
+++ Helloworld/src/com/usnschool/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("git test edit1");
 		System.out.println("hotfix edit");
+		System.out.println("hotfix!");
 
 	}
 
